0ca42faa12abe7d3c5535504c6a48e9626a40ab6
Stanislav-She <116562198+Stanislav-She@users.noreply.github.com>
2023-01-21 21:52:59 +0000

    Deploying to gh-pages from @ Stanislav-She/goit-js-hw-09@5422fb1eadba236d86312481150a35e15798fc53 🚀

--- 01-color-switcher.12007483.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("#data-start");console.log(o);
-//# sourceMappingURL=01-color-switcher.12007483.js.map
--- 01-color-switcher.837866fd.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("#data-start");console.log(o)}();
-//# sourceMappingURL=01-color-switcher.837866fd.js.map
